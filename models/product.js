ec90620c7a06c8ce54eb73355c7ffc27ae9bfe0c
YehonatamBinyamini <yonile2106@gmail.com>
2022-12-28 12:30:08 +0200

    adding img, desc., price.

--- models/product.js
+++ models/product.js
@@ -1,9 +1,11 @@
 const products = [];
-
+const defaultImage = "https://cdn.pixabay.com/photo/2016/03/31/20/51/book-1296045_960_720.png";
 module.exports = class Product {
-    constructor(title, ){
+    constructor(title, price, description, image){
         this.title = title;
-
+        this.price = price;
+        this.description = description;
+        image ? this.image = image : this.image = defaultImage;
     }
 
     save() {
