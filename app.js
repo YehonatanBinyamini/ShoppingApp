c091d7870e72456bc2eea8a0707735043bde505a
YehonatamBinyamini <yonile2106@gmail.com>
2022-12-28 10:12:51 +0200

    adding products controller

--- app.js
+++ app.js
@@ -4,16 +4,16 @@ const bodyParser = require("body-parser");
 const app = express();
 app.set('view engine', 'ejs')
 
-const adminData = require('./routes/admin');
+const adminRouts = require('./routes/admin');
 const shopRoutes = require('./routes//shop');
 const path = require("path");
-const rootDir = require('./util/path');
+//const rootDir = require('./util/path');
 const exp = require("constants");
 
 app.use(bodyParser.urlencoded());
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/admin', adminData.routes);
+app.use('/admin', adminRouts);
 app.use(shopRoutes); // the head page
 
 app.use((req, res, next) => { //error page
--- routes/admin.js
+++ routes/admin.js
@@ -1,20 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const path = require("path");
-const rootDir = require("../util/path");
+//const rootDir = require("../util/path");
+const productControllers = require("../controllers/products");
 
-const products = [];
+router.get("/add-product", productControllers.getAddProduct);
 
-router.get("/add-product", (req, res, next) => {
-  //res.sendFile(path.join(rootDir, "views", "add-product.html"));
-  res.render("add-product", { pageTitle: "Add Product" });
-});
+router.post("/add-product", productControllers.postAddProduct);
 
-router.post("/add-product", (req, res, next) => {
-  products.push({ title: req.body.title });
-  console.log(products);
-  res.redirect("/");
-});
-
-exports.routes = router;
-exports.products = products;
+module.exports = router;
