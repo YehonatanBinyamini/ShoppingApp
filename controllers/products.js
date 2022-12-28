d9b38005da9c2fd836ff49b01f747d13836238bd
YehonatamBinyamini <yonile2106@gmail.com>
2022-12-28 10:41:11 +0200

    creating product model

--- controllers/products.js
+++ controllers/products.js
@@ -1,16 +1,20 @@
+const Product = require("../models/product");
+
+
 exports.getAddProduct = (req, res, next) => {
     //res.sendFile(path.join(rootDir, "views", "add-product.html"));
     res.render("add-product", { pageTitle: "Add Product" });
 }
-const products = [];
 
 exports.postAddProduct = (req, res, next) => {
-    products.push({ title: req.body.title });
-    console.log(products);
+    const product = new Product(req.body.title)
+    product.save();
+    console.log(product);
     res.redirect("/");
   }
 
 exports.getProducts = (req, res, next) => {
+    const products = Product.fetchAll();
     res.render('shop', {
       prods: products,
       pageTitle: 'Shop',
