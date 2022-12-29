89d75f299690b297dc0d99a1b13585d9c268c49e
YehonatamBinyamini <yonile2106@gmail.com>
2022-12-29 12:35:08 +0200

    creating admin product page

--- controllers/products.js
+++ controllers/products.js
@@ -1,26 +1,41 @@
 const Product = require("../models/product");
 
-
 exports.getAddProduct = (req, res, next) => {
-    //res.sendFile(path.join(rootDir, "views", "add-product.html"));
-    res.render("admin/add-product", { pageTitle: "Add Product" });
-}
+  //res.sendFile(path.join(rootDir, "views", "add-product.html"));
+  res.render("admin/add-product", {
+    //path of the file
+    pageTitle: "Add Product",
+    path: "/admin/add-product",
+  });
+};
+
+exports.getAdminProducts = (req, res, next) => {
+  res.render("admin/products", {
+    pageTitle: "Products Management",
+    path: "/admin/products",
+  });
+};
 
 exports.postAddProduct = (req, res, next) => {
-    const product = new Product(req.body.title, req.body.price, req.body.description, req.body.image)
-    product.save();
-    console.log(product);
-    res.redirect("/");
-  }
+  const product = new Product(
+    req.body.title,
+    req.body.price,
+    req.body.description,
+    req.body.image
+  );
+  product.save();
+  console.log(product);
+  res.redirect("/");
+};
 
 exports.getProducts = (req, res, next) => {
-    const products = Product.fetchAll();
-    res.render('shop/products-list', {
-      prods: products,
-      pageTitle: 'Shop',
-      path: '/',
-      hasProducts: products.length > 0,
-      activeShop: true,
-      productCSS: true
-    });
-  }
+  const products = Product.fetchAll();
+  res.render("shop/products-list", {
+    prods: products,
+    pageTitle: "Shop",
+    path: "/",
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true,
+  });
+};
--- routes/admin.js
+++ routes/admin.js
@@ -6,6 +6,8 @@ const productControllers = require("../controllers/products");
 
 router.get("/add-product", productControllers.getAddProduct);
 
+router.get("/products", productControllers.getAdminProducts);
+
 router.post("/add-product", productControllers.postAddProduct);
 
 module.exports = router;
