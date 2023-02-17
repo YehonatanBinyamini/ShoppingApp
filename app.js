6326fd9afc5552a84bb2aab61849201000c6a71e
YehonatamBinyamini <yonile2106@gmail.com>
2023-02-17 10:02:39 +0200

    install mongodb & app config

--- app.js
+++ app.js
@@ -6,70 +6,77 @@ app.set("view engine", "ejs");
 const adminRouts = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 const path = require("path");
-const sequelize = require("./util/database");
-
-//const rootDir = require('./util/path');
 const errorController = require("./controllers/error");
+const mongoConnect = require("./util/database").mongoConnect;
 
-const Product = require("./models/product");
-const User = require("./models/user");
-const Cart = require("./models/cart");
-const CartItem = require("./models/cart-item");
-const OrderItem = require("./models/order-item");
-const Order = require("./models/order");
+//const sequelize = require("./util/database");
+//const rootDir = require('./util/path');
+// const Product = require("./models/product");
+// const User = require("./models/user");
+// const Cart = require("./models/cart");
+// const CartItem = require("./models/cart-item");
+// const OrderItem = require("./models/order-item");
+// const Order = require("./models/order");
 
 app.use(express.urlencoded({ extended: true })); //solved the problem with body parser
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use((req, res, next) => {
-  User.findByPk(1)
-    .then((user) => {
-      req.user = user;
-      next();
-    })
-    .catch((err) => console.log(err));
-});
+// app.use((req, res, next) => {
+//   User.findByPk(1)
+//     .then((user) => {
+//       req.user = user;
+//       next();
+//     })
+//     .catch((err) => console.log(err)); 
+// });
 
 app.use("/admin", adminRouts);
 app.use(shopRoutes);
 
 app.use(errorController.get404); //error page
 
-Product.belongsTo(User, {
-  constraints: true,
-  onDelete:
-    "CASCADE" /*means that in deleting of user, his products will be deleted also */,
+mongoConnect(() => {
+  //console.log(getDB());
+  app.listen(3000);
 });
-User.hasMany(Product);
-User.hasOne(Cart);
-//Cart.belongsTo(User); //one direction is enough but without User.hasMany(..) it won't be the func createProduct..
-Cart.belongsToMany(Product, { through: CartItem });
-Product.belongsToMany(Cart, { through: CartItem });
-//Order.belongsTo(User);
-User.hasMany(Order);
-Order.belongsToMany(Product, { through: OrderItem });
 
-sequelize
-  // .sync({ force: true })
-  .sync()
-  .then((result) => {
-    return User.findByPk(1);
-  })
-  .then((user) => {
-    if (!user) {
-      return User.create({ name: "john", email: "j@n.il" });
-    }
-    return user;
-  })
-  .then((user) => {
-    user.getCart().then((cart) => {
-      if (!cart) {
-        return user.createCart();
-      }
-      return cart;
-    });
-  })
-  .then((cart) => {
-    app.listen(3000);
-  })
-  .catch((err) => console.log(err));
+//sequelize relations and configurations:
+
+// Product.belongsTo(User, {
+//   constraints: true,
+//   onDelete:
+//     "CASCADE" /*means that in deleting of user, his products will be deleted also */,
+// });
+// User.hasMany(Product);
+// User.hasOne(Cart);
+// //Cart.belongsTo(User); //one direction is enough but without User.hasMany(..) it won't be the func createProduct..
+// Cart.belongsToMany(Product, { through: CartItem });
+// Product.belongsToMany(Cart, { through: CartItem });
+// //Order.belongsTo(User);
+// User.hasMany(Order);
+// Order.belongsToMany(Product, { through: OrderItem });
+
+// sequelize
+//   // .sync({ force: true })
+//   .sync()
+//   .then((result) => {
+//     return User.findByPk(1);
+//   })
+//   .then((user) => {
+//     if (!user) {
+//       return User.create({ name: "john", email: "j@n.il" });
+//     }
+//     return user;
+//   })
+//   .then((user) => {
+//     user.getCart().then((cart) => {
+//       if (!cart) {
+//         return user.createCart();
+//       }
+//       return cart;
+//     });
+//   })
+//   .then((cart) => {
+//     app.listen(3000);
+//   })
+//   .catch((err) => console.log(err));
--- models/product.js
+++ models/product.js
@@ -1,26 +1,80 @@
-const sequelize = require("../util/database");
-const { Sequelize } = require("sequelize");
+class Product {
+    constructor(id, title, price, description, image) {
+      this.id = id;
+      this.title = title;
+      this.price = price;
+      this.description = description;
+      image ? (this.image = image) : (this.image = defaultImage);
+    }
+  
+    save() {
+      getProductsFromFile((products) => {
+        if (this.id) {
+          const existingProductIndex = products.findIndex(
+            (prod) => prod.id === this.id
+          );
+          const updatedProducts = [...products];
+          updatedProducts[existingProductIndex] = this;
+          fs.writeFile(p, JSON.stringify(updatedProducts), (err) => {
+            console.log(err);
+          });
+        } else {
+          this.id = Math.floor(Math.random() * 100000000).toString();
+          //products.push(this);
+          products.push(this);
+          fs.writeFile(p, JSON.stringify(products), (err) => {
+            console.log(err);
+          });
+        }
+      });
+    }
+  
+    static deleteByID(id) {
+      getProductsFromFile((products) => {
+        //const deletedProductIndex = products.findIndex(p => p.id === id)
+        const updatedProducts = products.filter((prod) => prod.id !== id);
+        const deletedProduct = products.find((prod) => prod.id === id);
+        const productPrice = deletedProduct.price;
+        fs.writeFile(p, JSON.stringify(updatedProducts), (err) => {
+          if (!err) {
+            Cart.deleteProduct(id, productPrice);
+          } else console.log(err);
+        });
+      });
+    }
+  
+    static fetchAll(cb) {
+      getProductsFromFile(cb);
+    }
+  
+    static findByID(ID, cb) {
+      getProductsFromFile((products) => {
+        const product = products.find((p) => p.id === ID);
+        cb(product);
+      });
+    }
+  };
 
-const Product = sequelize.define("product", {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true,
-  },
-  title: Sequelize.STRING,
-  price: {
-    type: Sequelize.DOUBLE,
-    allowNull: false,
-  },
-  image: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  },
-  description: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  },
-});
+// const Product = sequelize.define("product", {
+//   id: {
+//     type: Sequelize.INTEGER,
+//     autoIncrement: true,
+//     allowNull: false,
+//     primaryKey: true,
+//   },
+//   title: Sequelize.STRING,
+//   price: {
+//     type: Sequelize.DOUBLE,
+//     allowNull: false,
+//   },
+//   image: {
+//     type: Sequelize.STRING,
+//     allowNull: false,
+//   },
+//   description: {
+//     type: Sequelize.STRING,
+//     allowNull: false,
+//   },
+// });
 
-module.exports = Product;
+// module.exports = Product;
